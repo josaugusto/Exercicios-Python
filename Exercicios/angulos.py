82f2bddd5cadfcb4047af39e84d1a3b447eec092
José Augusto <79456742+josaugusto@users.noreply.github.com>
2021-02-22 10:53:14 -0300

    Add files via upload

---
+++ Exercicios/angulos.py
@@ -0,0 +1,15 @@
+import math
+
+'''
+    Faça um programa que leia um ângulo qualquer
+    e mostre na tela o valor do seno, cosseno e tangente desse ângulo
+
+'''
+angulo = float(input('Informe o angulo em graus: '))
+rad = math.radians(angulo)
+seno = math.sin(rad)
+cosseno = math.cos(rad)
+tangente = math.tan(rad)
+print('O seno do {} ° é: {:.2}'.format(angulo, seno))
+print('O cosseno do {} ° é: {:.2}'.format(angulo, cosseno))
+print('A tangente do {} ° é: {:.2}'.format(angulo, tangente))
